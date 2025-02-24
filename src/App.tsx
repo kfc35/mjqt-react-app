f9159cf25a423407fecc8457a63aa07e1ac54f96
Kevin Chen <chen.kevin.f@gmail.com>
2025-02-24 18:12:11 -0500

    fix installation of scoring lib

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import MahjongTile from './mahjongTile/mahjongTile'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -24,9 +25,10 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <MahjongTile />
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite and React logos to learn more.
       </p>
     </>
   )
