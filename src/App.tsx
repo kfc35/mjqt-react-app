b371bd333d9ae13db3153bc8b1fdcacf430367dc
Kevin Chen <chen.kevin.f@gmail.com>
2025-02-26 15:59:33 -0500

    add disclaimer

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import MahjongKeyboard from './keyboard/MahjongKeyboard'
+import Footer from './Footer'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -23,6 +24,7 @@ function App() {
         </button>
         <MahjongKeyboard />
       </div>
+      <Footer />
     </>
   )
 }
---
+++ src/Footer.tsx
@@ -0,0 +1,18 @@
+function Footer() {
+    return (
+    <>
+        <footer>
+        <p>
+        Disclaimer<br />
+        This website was created just for fun and with love to the NYC Mahjong Community!<br />
+        This website and its utilities are intended for educational and recreational purposes only. <br />
+        This website does not claim its content and utilities to be complete, correct, or accurate. (The world of Mahjong is too large with so many variations... and honestly, there is probably a bug or two.)<br />
+        This website does not accept liability for any loss or damages arising from its usage or referencing, either directly or indirectly. By using this website, you use it at your own risk.<br />
+        If you or someone you know is struggling with a gambling problem, please contact the <a href="https://www.ncpgambling.org/help-treatment/" target="_blank">National Problem Gambling Helpline</a>.
+        </p>
+        </footer>
+    </>
+    )
+}
+
+export default Footer;
