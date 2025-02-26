1b6b92ca81722779149d21f9175134714cea83d0
Kevin Chen <chen.kevin.f@gmail.com>
2025-02-26 03:34:02 -0500

    start impl MahjongTile

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import MahjongTile from './mahjongTile/mahjongTile'
+import MahjongTile from './mahjongTile/MahjongTile'
 
 function App() {
   const [count, setCount] = useState(0)
