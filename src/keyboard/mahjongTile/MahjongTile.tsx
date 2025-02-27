90783fd1b315c22f79040c5f5fe61da77d0da778
Kevin Chen <chen.kevin.f@gmail.com>
2025-02-27 00:58:44 -0500

    add styling to buttons in mj keyboard

--- src/keyboard/mahjongTile/MahjongTile.tsx
+++ src/keyboard/mahjongTile/MahjongTile.tsx
@@ -1,18 +1,36 @@
 //import { useState } from 'react'
 import './MahjongTile.css'
-import { Tile } from 'mjqt-scoring'
+import { Tile, GREEN_TILES, REVERSIBLE_TILES, SIMPLE_TILES, TERMINAL_TILES } from 'mjqt-scoring'
 import mahjongTileToUnicodeMap from './mahjongTileUnicodeMap'
+
+const greenTilesSet: Set<Tile> = new Set(GREEN_TILES);
+const reversibleTilesSet: Set<Tile> = new Set(REVERSIBLE_TILES);
+const simpleTilesSet: Set<Tile> = new Set(SIMPLE_TILES);
+const terminalTilesSet: Set<Tile> = new Set(TERMINAL_TILES);
 
 interface MahjongTileProps {
   tile: Tile;
 }
 
 function MahjongTile(props: MahjongTileProps) {
-  const id = props.tile.group + "-" + props.tile.value;
-
+  const id = (props.tile.group + "-" + props.tile.value).toLowerCase();
+  const classNames: string[] = [];
+  classNames.push("mahjong-tile-button");
+  if (simpleTilesSet.has(props.tile)) {
+    classNames.push("simple-tile");
+  }
+  if (terminalTilesSet.has(props.tile)) {
+    classNames.push("terminal-tile");
+  }
+  if (greenTilesSet.has(props.tile)) {
+    classNames.push("green-tile");
+  }
+  if (reversibleTilesSet.has(props.tile)) {
+    classNames.push("reversible-tile");
+  }
   return (
     <>
-      <button className="mahjong-tile-button" id={id}>
+      <button className={classNames.join(" ")} id ={id} >
         {mahjongTileToUnicodeMap.get(props.tile)}
       </button>
     </>
