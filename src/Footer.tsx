e282d1d53c5477b88f210d16ab86ebd1731a4c19
Kevin Chen <chen.kevin.f@gmail.com>
2025-02-27 14:28:37 -0500

    remove some boilerplate, add favicon

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,3 +1,6 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
 function Footer() {
     return (
     <>
@@ -9,6 +12,16 @@ function Footer() {
         This website does not claim its content and utilities to be complete, correct, or accurate. (The world of Mahjong is too large with so many variations... and honestly, there is probably a bug or two.)<br />
         This website does not accept liability for any loss or damages arising from its usage or referencing, either directly or indirectly. By using this website, you use it at your own risk.<br />
         If you or someone you know is struggling with a gambling problem, please contact the <a href="https://www.ncpgambling.org/help-treatment/" target="_blank">National Problem Gambling Helpline</a>.
+        </p>
+        <p>
+        Made with 
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        and 
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
         </p>
         </footer>
     </>
